ec06995746e8ed12defdcdfd0698aedafc6d0af4
eparionad <eparionad@gmail.com>
2019-10-07 18:31:29 -0500

    se completo formulario

--- js/formualrio.js
+++ js/formualrio.js
@@ -3,7 +3,7 @@ function capturar(){
     var dato1 = document.getElementById("apellido").value;
     var dato2 = document.getElementById("email").value;
     var dato3 = document.getElementById("mensaje").value;
-    document.getElementById("resultado").innerHTML = " Nombres: "+dato+"<br>"+"Apellidos: "+dato1+"<br>"+"Email: "+dato2+"<br>"+"Mensaje: "+dato3;
+    document.getElementById("resultado").innerHTML = " Nombres: "+dato+"<br>"+"Apellidos: "+dato1+"<br>"+"Email: "+"<ins>"+dato2+"</ins>"+"<br>"+"Mensaje: "+dato3;
 }
 
 //capturar();
