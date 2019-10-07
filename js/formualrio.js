1a15a3d4ac3dc4c2d8461d4865b2dd147aa8a5b7
Ernesto Pariona Díaz <eparionad@gmail.com>
2019-10-07 17:08:22 -0500

    se crea el formulario

---
+++ js/formualrio.js
@@ -0,0 +1,11 @@
+function capturar(){
+    var dato = document.getElementById("nombre").value;
+    var dato1 = document.getElementById("apellido").value;
+    var dato2 = document.getElementById("email").value;
+    var dato3 = document.getElementById("mensaje").value;
+    document.getElementById("resultado").innerHTML = " Nombres: "+dato+"<br>"+"Apellidos: "+dato1+"<br>"+"Email: "+dato2+"<br>"+"Mensaje: "+dato3;
+}
+
+//capturar();
+
+var darClic = document.getElementById("boton").addEventListener("click", capturar);
